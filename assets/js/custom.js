46c11830851caba03b9cbb63a4137d9e39a58356
Kieran <kieran.summerfeldt@gmail.com>
2017-03-09 18:07:14 -0800

    home page method list

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -31,4 +31,47 @@ $(function() {
           modal.style.display = "none";
       }
   }
+
+  var wrap = $(".home");
+  var idea = $(".idea");
+  var discussion = $(".discussion");
+  var mockup = $(".mockup");
+  var website = $(".website");
+  var methodContainer = $(".method-list");
+
+  wrap.on("scroll", function(e) {
+    this.scrollTop > 450 ? firstItem(this) : idea.removeClass("visible");
+
+    function firstItem(object) {
+      idea.addClass("visible");
+      object.scrollTop > 785 ? fixList(object) : methodContainer.removeClass("fixed");
+    }
+
+    function fixList(object) {
+      methodContainer.addClass("fixed");
+      object.scrollTop > 820 ? secondItem(object) : discussion.removeClass("visible");
+    }
+
+    function secondItem(object) {
+      discussion.addClass("visible");
+      object.scrollTop > 1200 ? thirdItem(object) : mockup.removeClass("visible");
+    }
+
+    function thirdItem(object) {
+      mockup.addClass("visible");
+      object.scrollTop > 1400 ? fourthItem(object) : website.removeClass("visible");
+    }
+
+    function fourthItem(object) {
+      website.addClass("visible");
+    }
+
+  });    
+
 });
+
+
+
+
+
+
